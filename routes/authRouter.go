5708f2c706d053e0bf2f29a8dd56cfc584eb8478
KiranDesh <52851082+KiranDesh@users.noreply.github.com>
2023-03-09 17:35:40 +0530

    Update authRouter.go

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,11 +1,14 @@
 package routes
 
-import(
-	controller "github.com/KiranDesh/ecommece/controllers"
+import (
+	"github.com/KiranDesh/ecommece/controllers"
 	"github.com/gin-gonic/gin"
 )
 
-func AuthRoutes(incomingRoutes *gin.Engine){
-	incomingRoutes.POST("users/signup", controller.Signup())
-	incomingRoutes.POST("users/login", controller.Login())
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("/users/search", controllers.SearchProductByQuery())
 }
