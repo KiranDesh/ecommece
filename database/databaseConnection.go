f1ef380283e169828f7ee1026a9ed0cbb6faf9f0
KiranDesh <52851082+KiranDesh@users.noreply.github.com>
2023-03-09 17:33:25 +0530

    Update databaseConnection.go

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -1,25 +1,26 @@
 package database
 
-import(
-"fmt"
-"log"
-"time"
-"os"
-"context"
-"github.com/joho/godotenv"
-"go.mongodb.org/mongo-driver/mongo"
-"go.mongodb.org/mongo-driver/mongo/options"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func DBinstance() *mongo.Client{
+func DBinstance() *mongo.Client {
 	err := godotenv.Load(".env")
-	if err!=nil{
+	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
 	MongoDb := os.Getenv("MONGODB_URL")
 
-	client, err:= mongo.NewClient(options.Client().ApplyURI(MongoDb))
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -30,6 +31,11 @@ func DBinstance() *mongo.Client{
 	if err != nil {
 		log.Fatal(err)
 	}
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Println("failed to connect to mongodb")
+		return nil
+	}
 	fmt.Println("Connected to MongoDB!")
 
 	return client
@@ -37,7 +43,11 @@ func DBinstance() *mongo.Client{
 
 var Client *mongo.Client = DBinstance()
 
-func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection{
-	var collection *mongo.Collection = client.Database("cluster0").Collection(collectionName)
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
 	return collection
+}
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var productcollection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return productcollection
 }
