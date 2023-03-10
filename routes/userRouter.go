04da685d0079f9d9a662e14e1f1c0baff031a7db
KiranDesh <52851082+KiranDesh@users.noreply.github.com>
2023-03-10 10:14:58 +0530

    Update userRouter.go

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,12 +1,12 @@
 package routes
 
-import(
+import (
 	controller "github.com/KiranDesh/ecommece/controllers"
 	"github.com/KiranDesh/ecommece/middleware"
 	"github.com/gin-gonic/gin"
 )
 
-func UserRoutes(incomingRoutes *gin.Engine){
+func UserRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.Use(middleware.Authenticate())
 	incomingRoutes.GET("/users", controller.GetUsers())
 	incomingRoutes.GET("/users/:user_id", controller.GetUser())
