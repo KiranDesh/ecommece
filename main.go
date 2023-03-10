676b01da874b486f17c9042be560437ab83b80fd
KiranDesh <52851082+KiranDesh@users.noreply.github.com>
2023-03-10 10:17:05 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,33 +4,36 @@ import (
 	"log"
 	"os"
 
-	"github.com/KiranDesh/ecommece/controllers"
-	"github.com/KiranDesh/ecommece/database"
-	"github.com/KiranDesh/ecommece/middleware"
-	"github.com/KiranDesh/ecommece/routes"
-
+	routes "github.com/KiranDesh/ecommece/routes"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	port := os.Getenv("PORT")
+
 	if port == "" {
 		port = "8000"
 	}
-	app := controllers.NewApplication(database.ProductData(database.Client, "Products"), database.UserData(database.Client, "Users"))
 
 	router := gin.New()
 	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
 	routes.UserRoutes(router)
-	router.Use(middleware.Authenticate())
-	router.GET("/addtocart", app.AddToCart())
-	router.GET("/removeitem", app.RemoveItem())
-	router.GET("/listcart", controllers.GetItemFromCart())
-	router.POST("/addaddress", controllers.AddAddress())
-	router.PUT("/edithomeaddress", controllers.EditHomeAddress())
-	router.PUT("/editworkaddress", controllers.EditWorkAddress())
-	router.GET("/deleteaddresses", controllers.DeleteAddress())
-	router.GET("/cartcheckout", app.BuyFromCart())
-	router.GET("/instantbuy", app.InstantBuy())
-	log.Fatal(router.Run(":" + port))
+
+	router.GET("/api-1", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-1"})
+	})
+
+	router.GET("/api-2", func(c *gin.Context) {
+		c.JSON(200, gin.H{"success": "Access granted for api-2"})
+	})
+
+	router.Run(":" + port)
 }
